0e4b9bd999140fb0a9a2b1f1ce5d2b1135a3ae26
Gilberto Olimpio dos Santos <c129427@corp.caixa.gov.br>
2020-01-14 15:00:59 -0300

    Revert "comiit"
    This reverts commit 11533e5987c49ef65ec066d3f174c17965ed4cd3.

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import express from "express";
-import consign from "consign";
+const PORT = 3000;
 
 const app = express();
 
-consign()
-    .include("libs/config.js")
-    .then("db.js")
-    .then("libs/middlewares.js")
-    .then("routes")
-    .then("libs/boot.js")
-    .into(app);
+app.get("/", (req, res) => res.json({status: "NTask API"}));
+
+app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
