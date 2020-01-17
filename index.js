22a32d3968aee350b81478c4f04a8cd95afd7e17
c129427 <olimpiogilberto@gmail.com>
2020-01-17 19:41:29 -0300

    tasks basicas
    Nesta etapa é possível criar um serviço básico de tasks, porém não roda com nodemon.

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+// import express from "express";
+// import consign from "consign";
+
+// const app = express();
+
+// consign()
+//     .include("libs/config.js")
+//     .then("db.js")
+//     .then("libs/middlewares.js")
+//     .then("routes")
+//     .then("libs/boot.js")
+//     .into(app);
+
+
+
 import express from "express";
-import consign from "consign";
+
+const PORT = 3000;
 
 const app = express();
 
-consign()
-    .include("libs/config.js")
-    .then("db.js")
-    .then("libs/middlewares.js")
-    .then("routes")
-    .then("libs/boot.js")
-    .into(app);
+app.set("json spaces", 4);
+    
+app.get("/", (req, res) => {res.json(
+    {status: "NTask API"});
+console.log(`A pasta / foi acessada!!`);
+    
+});
+
+app.get("/tasks", (req, res) => {
+ res.json({
+     tasks: [
+        {title: "Fazer compras"},
+        {title: "Consertar o pc"},
+     ]
+    
+    }) ;
+
+    console.log(`A pasta tasks foi acessada!!`);
+});
+
+app.listen(PORT, () => {
+    console.log(`NTask API - porta ${PORT}`);
+});
