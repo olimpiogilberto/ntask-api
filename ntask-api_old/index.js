47334cf056863f9781a3460107ed05f8437a0e18
Gilberto Olimpio dos Santos <c129427@corp.caixa.gov.br>
2020-07-06 19:32:58 -0300

    alteração de pastas
    feito

---
+++ ntask-api_old/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import consign from "consign";
+
+const app = express();
+
+consign()
+    .include("src/libs/config.js")
+    .then("db.js")
+    .then("src/auth.js")
+    .then("src/libs/middlewares.js")
+    .then("src/routes")
+    .then("src/libs/boot.js")
+    .into(app);
+
