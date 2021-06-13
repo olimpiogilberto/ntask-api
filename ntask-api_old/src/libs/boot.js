c3692ca2ab1d8e8ae0587ce4f620a909b9c04c99
olimpiogilberto <olimpiogilberto@hotmail.com>
2021-06-13 15:37:46 -0300

    Revert "alteração de pastas"
    This reverts commit 47334cf056863f9781a3460107ed05f8437a0e18.

--- ntask-api_old/src/libs/boot.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    app.db.sequelize.sync().done(() => {
-        app.listen(app.get("port"), () => {
-            console.log(`boot.js - API Startada na porta ${app.get("port")}`);
-        });
-    });
-}
